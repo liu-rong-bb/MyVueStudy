e20623beb8f3ca4797b03c4f52cf4833db2c4182
刘容 <2287383251@qq.com>
2022-06-07 10:13:42 +0800

    新建Vue脚手架项目

---
+++ vue-two-study/src/App.vue
@@ -0,0 +1,22 @@
+<!--汇总所有的组件-->
+<template>
+    <div>
+        <School></School>
+    </div>
+</template>
+
+<script>
+
+    import School from './components/School'
+
+    export default {
+        name: "App",
+        components: {
+            School
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-two-study/src/main.js
@@ -0,0 +1,24 @@
+// 引用vue
+import Vue from 'vue'
+// app组件是所有组件的父组件
+import App from './App.vue'
+
+// 关闭Vue中的生产提示
+Vue.config.productionTip = false
+
+// 创建Vue实例对象
+new Vue({
+  el: '#app',
+
+  // render(h) {
+  //   return h(App)
+  // }
+  // 没有用到this就可以用箭头函数
+  render:h => h(App)
+
+  // template:'<App></App>',
+  // components: {App},
+  // 将app组件放入容器中
+  // render: h => h(App),
+})
+    // .$mount('#app')
